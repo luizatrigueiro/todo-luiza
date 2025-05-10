881cb325bb83f3785965ae0687c354a8b27c71d0
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-05-10 12:29:09 -0300

    hotfix: corrected formatting errors

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,17 @@
 class ListsController < ApplicationController
-    before_action :set_list, only: [:show, :edit, :update, :destroy]
-  
+    before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+
     def index
       @lists = List.all
     end
-  
+
     def show
     end
-  
+
     def new
       @list = List.new
     end
-  
+
     def create
       @list = List.new(list_params)
       if @list.save
@@ -20,10 +20,10 @@ class ListsController < ApplicationController
         render :new, status: :unprocessable_entity
       end
     end
-  
+
     def edit
     end
-  
+
     def update
       if @list.update(list_params)
         redirect_to @list, notice: "List successfully updated!"
@@ -31,20 +31,19 @@ class ListsController < ApplicationController
         render :edit, status: :unprocessable_entity
       end
     end
-  
+
     def destroy
       @list.destroy
       redirect_to lists_path, notice: "List successfully deleted!"
     end
-  
+
     private
-  
+
     def set_list
       @list = List.find(params[:id])
     end
-  
+
     def list_params
       params.require(:list).permit(:title)
     end
-  end
-  
+end
